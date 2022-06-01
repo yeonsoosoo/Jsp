1a5fd23ebf8065ea6803cd63f8e344874bc62fb3
Park Yeonsoo <lovepys11@naver.com>
2022-06-01 17:30:46 +0900

    Database

--- Ex_0601_gallery/src/dao/PhotoDAO.java
+++ Ex_0601_gallery/src/dao/PhotoDAO.java
@@ -29,7 +29,7 @@ public class PhotoDAO {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String sql = "select * from photo";
+		String sql = "select * from photo order by idx desc";
 
 		try {
 			//1.Connection얻어온다
@@ -46,8 +46,8 @@ public class PhotoDAO {
 				vo.setIdx(rs.getInt("idx"));
 				vo.setTitle(rs.getString("title"));
 				vo.setFilename(rs.getString("filename"));
-				vo.setIp(rs.getString("ip"));
 				vo.setPwd(rs.getString("pwd"));
+				vo.setIp(rs.getString("ip"));
 				vo.setRegidate(rs.getString("regidate"));
 
 				//ArrayList추가
@@ -73,5 +73,86 @@ public class PhotoDAO {
 		}
 
 		return list;
+	} // select
+	
+	public int insert(PhotoVO vo) {
+		// TODO Auto-generated method stub
+		int res = 0;
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		String sql = "insert into photo values(seq_photo_idx.nextval, ?, ?, ?, ?,sysdate)";
+
+		try {
+			//1.Connection획득
+			conn = DBService.getInstance().getConnection();
+			//2.명령처리객체 획득
+			pstmt = conn.prepareStatement(sql);
+
+			//3.pstmt parameter 채우기
+			pstmt.setString(1, vo.getTitle());
+			pstmt.setString(2, vo.getFilename());
+			pstmt.setString(3, vo.getPwd());
+			pstmt.setString(4, vo.getIp());
+			
+			//4.DB로 전송(res:처리된행수)
+			res = pstmt.executeUpdate();
+			
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return res;
+	}
+	
+	public int delete(int idx) {
+		// TODO Auto-generated method stub
+		int res = 0;
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		String sql = "delete from photo where idx=?";
+
+		try {
+			//1.Connection획득
+			conn = DBService.getInstance().getConnection();
+			//2.명령처리객체 획득
+			pstmt = conn.prepareStatement(sql);
+
+			//3.pstmt parameter 채우기
+			pstmt.setInt(1, idx);
+			//4.DB로 전송(res:처리된행수)
+			res = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return res;
 	}
 }
